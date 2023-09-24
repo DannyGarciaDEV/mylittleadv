cae0c31db57bd5a57143bccb9eca640c635041d1
Danny Garcia Cortes <dannygarciadev@gmail.com>
2023-09-24 12:50:26 -0400

    addin last update

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,8 +5,10 @@ var map = L.map('map', {
         L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         })
-    ]
+    ],
+    zoomControl:false
 });
+
 
 var userMarker;
 var userMarker2;
